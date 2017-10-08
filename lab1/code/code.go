d10114b2673954405758f1d0c60b3ee94c8a63aa
Viktoriia Brykalova <viktorytori1810@gmail.com>
2017-10-08 18:21:10 +0300

    added lab1

---
+++ lab1/code/code.go
@@ -0,0 +1,79 @@
+
+package code
+
+
+func F1(line string, symb byte) (int, bool){  
+	line_len := len(line)
+	for i:=0; i<line_len; i++{
+		if line[i] == symb{
+			return i, false
+		}
+	} 
+	return 0, true
+}
+
+
+func F2(mask_part string) (int, bool){
+	mask_part_len := len(mask_part)
+	var i int
+	for i:=0; i<mask_part_len; i++{
+		if mask_part[i] != '*'{
+			return i, false
+		}
+	}
+	return i, true
+}
+
+
+func F3(mask string, line string, k int) (int, bool){
+	mask_len:=len(mask); line_len:=len(line); full_mask:=false; error:=false;
+	for j:= 0; j<mask_len; j++{  
+		if (mask[j] == '*'){ 
+			help := 0; symb := 0
+			symb, full_mask = F2(mask[j:])
+			if full_mask == true{
+				k = line_len
+				break
+			}
+			help, error = F1(line[k:], mask[j+symb])
+			if error == false{
+				k = k + help
+				k++
+				j= j+symb
+			}else{break}
+		}else if line[k] != mask[j]{
+			error = true; break;
+		}else {k++}
+	}
+	return k, error
+}
+
+
+
+func Task(line string, mask string) string{
+	line_len := len(line); 
+	result:=""; 
+	for i:=0; i<line_len; i++{  
+		k:=i; 
+		error:=false; 
+		start_index:=i; 
+		k, error = F3(mask, line, k)
+		if error == false{  
+			result =result + "  " + line[start_index: k]
+		}
+	}
+	return result
+}
+
+
+
+
+
+
+
+
+// func main(){
+// 	// fmt.Println(task("ba bbbac", "b*a**"))   
+// }
+
+
---
+++ lab1/m_test.go
@@ -0,0 +1,77 @@
+
+package main
+
+
+import(	"testing"
+ 		"io/ioutil"
+		"labs/lab1/code"
+)
+
+func TestTaskFull(t *testing.T) {
+	
+    contents,_ := ioutil.ReadFile("tests.txt")
+	var str = string(contents)
+        
+    ioutil.WriteFile("output.txt", []byte(code.Task(str, "a*****b*****c")), 0644)
+}
+
+
+func TestTask(t *testing.T){
+	res := code.Task("adsb fg afihfiuhfpb", "a***b")
+	if res != "  adsb  afihfiuhfpb"{
+		t.Error( 
+            "expected", "  adsb  afihfiuhfpb",
+            "got", res,
+        )
+	}
+}
+
+
+func TestF1(t *testing.T) {
+	res, flag := code.F1("abc", 'c')
+	if 2 != res{
+        t.Error( 
+            "expected", 2,
+            "got", res,
+        )
+    }
+    if flag != false{
+    	t.Error( 
+            "expected", false,
+            "got", flag,
+        )
+    }
+}
+
+
+func TestF2(t *testing.T){
+	res, flag:= code.F2("***b")
+	if 3 != res {
+        t.Error( 
+            "expected", 3,
+            "got", res,
+        )
+    }
+    if flag != false{
+    	t.Error( 
+            "expected", false,
+            "got", flag,
+        )
+    }
+}
+
+func TestF3(t *testing.T){
+	res, flag := code.F3("a*d", "abcd", 0)
+	if 4 != res {
+        t.Error( 
+            "expected", 4,
+            "got", res,
+        )
+    }
+    if flag != false{
+    	t.Error( 
+            "expected", false,
+            "got", flag,
+        )
+    }
+}
